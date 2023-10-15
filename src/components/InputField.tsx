c29bb5a59421747e12c6aa43fca9173831bd9665
Harriet Oteng <harriet.oteng@gmail.com>
2023-10-15 21:23:43 +0000

    Add states for InputField component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+// const InputField: React.FC<Props> = ({ todo, setTodo })
+const InputField = ({ todo, setTodo, handleAdd }: Props) => {
   return (
-    <form className="input">
-      <input type="text" placeholder="Enter a task" className="input__box" name="" id="" />
+    <form className="input" onSubmit={(e) => handleAdd(e)}>
+      <input 
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="input__box"
+        name=""
+        id=""
+      />
       <button type="submit" className="input__submit">Go</button>
     </form>
   )
